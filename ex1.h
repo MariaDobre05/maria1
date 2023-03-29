55d6e8078a1f38e66a44feccb8ff17981f084f1f
maria dobre <cinema@cinema.com>
2023-03-29 10:50:08 +0300

    laborator 5 stive +cozi

---
+++ ex1.h
@@ -0,0 +1,1 @@
+Data top(Node **top,Data v
---
+++ stive.c
@@ -0,0 +1,14 @@
+#include "ex1.h"
+
+
+Data top(Node **top,Data v)
+{
+    if(isEmpty(top))
+        return CHAR_MIN;
+    return top->val;
+}
+
+void push (Node **top, Data v)
+{
+
+}
